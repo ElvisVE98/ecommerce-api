ce3176561a272467f70e489f4a3a2af7b6bc3ddc
ElvisV98 <elv.velasquez@duocuc.cl>
2026-04-20 21:22:11 -0400

    Pages CatalogPages se agrega filtros y barra de busqueda funcional

--- frontend/src/pages/CatalogPage.tsx
+++ frontend/src/pages/CatalogPage.tsx
@@ -8,12 +8,21 @@ import ProductCard from '../components/ProductCard';
 
 
 
+
 const CatalogPage = () =>{
 
     const [products,setProducts] = useState<Product[]>([]);
     const [categories,setCategories] = useState<Category[]>([]);
     const [loading,setLoading] = useState(true);
+    const [selectedCategory,setSelectedCategory] = useState<string>('all');
+    const [searchQuery,setSearchQuery] = useState('');
 
+   
+    
+    const filteredProducts = products
+  .filter(p => selectedCategory === 'all' || p.category_id === selectedCategory)
+  .filter(p => p.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    
     useEffect (() =>{
 
         const fetchData = async() =>{
@@ -49,12 +58,50 @@ if(loading){
                 <h1 className='text-3xl font-bold text-slate-800 mb-2'>Nuestra Colección</h1>
                 <p className='text-slate-500 mb-8'>Explora todos nuestros Productos</p>
 
+            {/* Filtro por categoría - dos columnas */}
+
+            <div className='flex gap-8'>
+
+            {/* Columna izquierda — filtros */}
+                <div className='w-48 shrink-0'>
+                    <p className='text-xs font-semibold uppercase tracking-wide text-slate-500 mb-3'>Categorias</p>
+
+                    <button
+                        onClick={() => setSelectedCategory('all')}
+                        className={`w-full text-left px-3 py-2 rounded-lg text-sm mb-1 transition ${selectedCategory === 'all'? 'bg-slate-800 text-white': 'text-slate-600 hover:bg-slate-100'}`}>
+                        Todo
+                    </button>
 
 
-                {/*Cards para ver los productos */}
 
+                    {/* Botones de cada categoría */}
+                     {categories.map((category) => (
+                    <button
+                    key={category.id}
+                    onClick={() => setSelectedCategory(category.id)}
+                    className={`w-full text-left px-3 py-2 rounded-lg text-sm mb-1 transition
+                    ${selectedCategory === category.id
+                    ? 'bg-slate-800 text-white'
+                    : 'text-slate-600 hover:bg-slate-100'
+                    }`}>
+                {category.name}
+                </button>
+                ))}
+                </div>
+
+
+            
+
+               {/* Columna derecha — productos */}
+                <div className='flex-1'>
+                    <input type="text"
+                    placeholder='Buscar Productos'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className='w-full border border-slate-200 rounded-lg px-4 py-2 text-sm mb-6 focus:outline-none focus:ring-2 focus:ring-blue-500' 
+                    />
                 <div className='grid grid-cols-3 gap-6'>
-                    {products.map((product) =>{
+                    {filteredProducts.map((product) =>{
                         const category = categories.find(c => c.id === product.category_id);
                         return(
                         <ProductCard 
@@ -65,10 +112,10 @@ if(loading){
                         );
                      })}
                 </div>
-
-
-            </main>
-            <Footer />
+            </div>
+        </div>
+        </main>
+    <Footer />
         </div>        
 
     );
