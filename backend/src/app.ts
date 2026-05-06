0d9b2da906d741d6a992d465300b2aa7ac1987f4
elvis velasquez <evelasquez_98@hotmail.com>
2026-05-06 14:40:06 -0400

    se modifica cors en backend para agregar url de netlify

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const app = express(); // aqui crea la aplicacion, es el servidor en si, es el o
 app.use(express.json()); //esto sirve para que el servidor pueda entender las peticiones con cuerpo en formato JSON, es decir, para parsear el cuerpo de las peticiones entrantes como JSON y ponerlo a disposición en req.body
 
 app.use(cors({
-    origin: 'http://localhost:5173', // esto es para permitir que el frontend pueda hacer peticiones al backend, es decir, para permitir que el frontend pueda acceder a los recursos del backend, en este caso, el frontend está corriendo en localhost:5173, por lo que se permite el acceso desde esa URL, si el frontend estuviera corriendo en otra URL, se tendría que cambiar esta configuración para permitir el acceso desde esa URL
+    origin: [ 'http://localhost:5173','https://mercadoecommerce.netlify.app'], // esto es para permitir que el frontend pueda hacer peticiones al backend, es decir, para permitir que el frontend pueda acceder a los recursos del backend, en este caso, el frontend está corriendo en localhost:5173, por lo que se permite el acceso desde esa URL, si el frontend estuviera corriendo en otra URL, se tendría que cambiar esta configuración para permitir el acceso desde esa URL
     credentials: true, // esto es para permitir que el frontend pueda enviar cookies al backend, es decir, para permitir que el frontend pueda enviar el token de autenticación al backend, esto es necesario para que el backend pueda identificar al usuario que está haciendo la petición y darle acceso a los recursos protegidos, si no se permite el envío de cookies, el backend no podrá identificar al usuario y no podrá darle acceso a los recursos protegidos
 }));
 
